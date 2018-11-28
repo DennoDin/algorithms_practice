76510609f84c65e0cc64dceaca5e72f3bb47bbd0
Dustin <dustin.tran@live.com>
2018-11-28 14:24:05 +0900

    created basic loop for bivalue function

---
+++ bivalueArrays.js
@@ -0,0 +1,12 @@
+/* 
+Determine the largest length that returns the largest bivalue array.
+A bivalue array is an array made of only two values (i.e. [1,2,2,1] || [3,4,3,4,4,3])
+*/
+
+let largestBivalue = (array) => {
+  let result = -1;                          //default: returns -1
+  for (let i = 0; i < array.length; i++){   //loops through entire array
+
+  }
+  return result;                            //returns result
+}
